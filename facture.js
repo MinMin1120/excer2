3baee4ba6a8b255a6169261c34c2f789ab085b99
MinMin1120 <144164468+MinMin1120@users.noreply.github.com>
2023-10-24 10:52:25 +0200

    Add files via upload

---
+++ facture.js
@@ -0,0 +1,22 @@
+function multiplication(){
+    var a= document.getElementById("a1").value;
+    var b= document.getElementById("b1").value;
+    var c= document.getElementById("a2").value;
+    var e= document.getElementById("b2").value;
+    var f= document.getElementById("a3").value;
+    var g= document.getElementById("b3").value;
+    var h= a*b;
+    var g= c*e;
+    var o= f*g;
+    var u=h+g+o;
+    c1.value=h;
+    c2.value=g;
+    c3.value=o;
+    d.value=u;
+}
+function Réinitiliser(){
+    window.location.reload();
+}
+
+
+
